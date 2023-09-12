24fbbe577b58c16fe1fa253b9f9a059375ceb1f0
lijun <1120317495@qq.com>
2023-09-12 15:53:21 +0800

    add comments

--- pid_simulation/pid_simulation.py
+++ pid_simulation/pid_simulation.py
@@ -1,21 +1,24 @@
 import numpy as np
 from matplotlib import pyplot as plt
 
+# autor: lijun
+# 增量式pid参考链接: https://www.cnblogs.com/zhj66/p/14307385.html
+
 
 class pid_control_data:
-    kp = 0
-    ki = 0
-    kd = 0
-    err = 0
-    last_err = 0
-    llast_eer = 0
-    power = 0
-    freq = 1000
-    delta = 0
-    target_power = 100
-    length_time = 60
-    start_time = 0
-    time = 0
+    kp = 0  # pid k参数
+    ki = 0  # pid i参数
+    kd = 0  # pid d参数
+    err = 0  # 系统输出与控制目标的当前误差
+    last_err = 0  # 系统输出与控制目标的上一次误差
+    llast_eer = 0  # 系统输出与控制目标的上上次误差
+    power = 0  # 记录系统输出功耗
+    freq = 1000  # 芯片初始频率
+    delta = 0  # 控制器增量值
+    target_power = 100  # 控制目标
+    length_time = 60  # 仿真时间长度
+    start_time = 0  # 开始时间
+    time = 0  # 时间
 
     def reset(self, kp, ki, kd):
         self.kp = kp
@@ -33,7 +36,7 @@ class pid_control_data:
         return
 
 
-def power_mode(pid_class):
+def power_mode(pid_class):  # 被控系统 有两个阶段 一阶段为功耗上升阶段 二阶段为功耗稳定阶段
     if (pid_class.time > pid_class.start_time):
         pid_class.power = 0.16 * pid_class.freq + 25
     else:
@@ -42,7 +45,7 @@ def power_mode(pid_class):
     return
 
 
-def pid_control(pid_class):
+def pid_control(pid_class):  # pid 控制器核心算法
     pid_class.llast_err = pid_class.last_err
     pid_class.last_err = pid_class.err
     pid_class.err = pid_class.target_power - pid_class.power
@@ -57,21 +60,21 @@ def pid_control(pid_class):
 
 
 def main():
-    pcon = pid_control_data()
+    pcon = pid_control_data()  # 初始化区开始
     length = pcon.length_time
     pid_delta_data = np.zeros(length)
     pid_freq_data = np.zeros(length)
     pid_power_data = np.zeros(length)
     pid_target_power = np.zeros(length)
     x = range(length)
-    param_kp = 0.2
-    param_ki = 5.3
+    param_kp = 0.1
+    param_ki = 5.4
     param_kd = 0
     fig1 = plt.figure(1)
-    fig2 = plt.figure(2)
-    for j in range(6):
+    fig2 = plt.figure(2)  # 初始化区结束
+    for j in range(6):  # 运行六次
         pcon.reset(param_kp, param_ki, param_kd)
-        for i in x:
+        for i in x:  # 运行开始
             power_mode(pcon)
             pid_control(pcon)
             pcon.freq += pcon.delta
@@ -83,8 +86,8 @@ def main():
             pid_target_power[i] = pcon.target_power
             pcon.time = pcon.time + 1
             if (pcon.time > 40):
-                pcon.target_power = 80
-        else:
+                pcon.target_power = 80  # 运行结束
+        else:                                     # 作图区开始
             tempfig = fig1.add_subplot(2, 3, j+1)
             tempfig.set_title("power fig{}      kp:{:.3f} - ki:{:.3f} - kd:{:.3f}".format(
                 j, pcon.kp, pcon.ki, pcon.kd))
@@ -104,8 +107,10 @@ def main():
             tempfig.set_ylim(0, 1200)
             tempfig.grid()
             tempfig.plot(x, pid_freq_data, color='b')
-        param_ki += 0.1
-    plt.show()
+        # param_ki += 0.2
+        param_kp += 0.2
+        # param_kd += 0.5
+    plt.show()      # 作图区结束， 显示图片
     return
 
 
